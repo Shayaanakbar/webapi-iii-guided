d18f16ce8ce92f6626f7457d039a70fbb438abb0
shayaanakbar <shayaanakbar1106@gmail.com>
2019-05-13 23:07:28 -0500

    Class

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -4,6 +4,12 @@ const Hubs = require('./hubs-model.js');
 const Messages = require('../messages/messages-model.js');
 
 const router = express.Router();
+const idBodyCheck = [validateId, requireBody];
+
+router.use((req, res, next) => {
+  console.log('something');
+  next();
+});
 
 // this only runs if the url has /api/hubs in it
 router.get('/', async (req, res) => {
@@ -21,25 +27,11 @@ router.get('/', async (req, res) => {
 
 // /api/hubs/:id
 
-router.get('/:id', async (req, res) => {
-  try {
-    const hub = await Hubs.findById(req.params.id);
-
-    if (hub) {
-      res.status(200).json(hub);
-    } else {
-      res.status(404).json({ message: 'Hub not found' });
-    }
-  } catch (error) {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error retrieving the hub',
-    });
-  }
+router.get('/:id', validateId, async (req, res) => {
+  res.status(200).json(req.hub);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', requireBody, async (req, res) => {
   try {
     const hub = await Hubs.add(req.body);
     res.status(201).json(hub);
@@ -52,7 +44,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', validateId, async (req, res) => {
   try {
     const count = await Hubs.remove(req.params.id);
     if (count > 0) {
@@ -69,7 +61,7 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', validateId, requireBody, async (req, res) => {
   try {
     const hub = await Hubs.update(req.params.id, req.body);
     if (hub) {
@@ -88,7 +80,7 @@ router.put('/:id', async (req, res) => {
 
 // add an endpoint that returns all the messages for a hub
 // this is a sub-route or sub-resource
-router.get('/:id/messages', async (req, res) => {
+router.get('/:id/messages', validateId, async (req, res) => {
   try {
     const messages = await Hubs.findHubMessages(req.params.id);
 
@@ -103,7 +95,7 @@ router.get('/:id/messages', async (req, res) => {
 });
 
 // add an endpoint for adding new message to a hub
-router.post('/:id/messages', async (req, res) => {
+router.post('/:id/messages', validateId, requireBody, async (req, res) => {
   const messageInfo = { ...req.body, hub_id: req.params.id };
 
   try {
@@ -117,5 +109,29 @@ router.post('/:id/messages', async (req, res) => {
     });
   }
 });
+
+
+async function validateId(req, res, next) {
+    try {
+      const { id } = req.params;
+      const hub = await Hubs.findById(id);
+      if (hub) {
+        req.hub = hub; // in all of these routes we will have access to req.hub
+      } else {
+        res.status(404).json({ message: 'Hub not found; invalid id'});
+      }
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to process request'});
+    }
+  }
+
+  function requireBody(req, res, next) {
+    if (req.body && Object.kets(req.body).length) {
+      next();
+    } else {
+      res.status(400).json({ message: "Please include request body"});
+    }
+  }
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
+const logger = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+// GLOBAL MIDDLEWARE
+
+// built in  middleware
 server.use(express.json());
 
+// third party middleware
+server.use(helmet());
+server.use(logger('dev'));
+
+// custom middleware
+server.use(typeLogger);
+server.use(addName);
+// server.use(lockout);
+// server.use(moodyGatekeeper);
+
+// router
 server.use('/api/hubs', hubsRouter);
 
+// this is custom middleware too but doesnt require next
 server.get('/', (req, res, next) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
 
@@ -16,5 +33,34 @@ server.get('/', (req, res, next) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+// custom middleware
+
+function typeLogger(req, res, next) {
+  console.log (`${req.method} Request`);
+  next();
+}
+
+function addName(req, res, next) {
+  req.name = req.name || "Shayaan";
+  next();
+}
+
+function lockout(req, res, next) {
+  res.status(403).json({ message: 'API lockout!'}
+  );
+}
+
+function moodyGatekeeper(req, res, next) {
+  const seconds = new Date().getSeconds();
+
+  if (seconds % 3 === 0) {
+    res.status(403).json({ message: "you shall not pass!"});
+  } else {
+    next();
+  }
+}
+
+
 
 module.exports = server;
